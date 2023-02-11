82741b69e8c9ec104a5b9f99637ad2471c90469a
Stephen <stephenbrady365@gmail.com>
2023-02-11 16:34:13 +0000

    Add dependency to allow jest to mock css

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 const config = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  moduleNameMapper: { "\\.(css|less)$": "identity-obj-proxy" },
 };
 module.exports = config;
